326420f935d959a908c59dcc60022707f5fd7ca6
Nattapon Sub-Anake <nattapon.subanake@gmail.com>
2019-10-20 15:29:36 +0700

    Add delete quote function

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import './style.dart';
 import './models/quote.dart';
+import './quote_card.dart';
 
 class App extends StatelessWidget {
   // RouteFactory _routes() {
@@ -91,46 +92,20 @@ class _QuoteListState extends State<QuoteList> {
           child: Column(
             // crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              ...quotes.map((quote) => QuoteCard(quote: quote)).toList(),
+              ...quotes
+                  .map(
+                    (quote) => QuoteCard(
+                      quote: quote,
+                      delete: () {
+                        setState(() {
+                          quotes.remove(quote);
+                        });
+                      },
+                    ),
+                  )
+                  .toList(),
             ],
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class QuoteCard extends StatelessWidget {
-  final Quote quote;
-
-  const QuoteCard({this.quote});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text(
-              quote.text,
-              style: TextStyle(
-                fontSize: 14.0,
-                fontWeight: FontWeight.w400,
-                color: Colors.grey[600],
-              ),
-            ),
-            SizedBox(height: 6.0),
-            Text(
-              quote.author,
-              style: TextStyle(
-                fontSize: 12.0,
-                color: Colors.grey[800],
-              ),
-            ),
-          ],
         ),
       ),
     );
