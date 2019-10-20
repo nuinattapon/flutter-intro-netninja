c30884fc2296b7935b763cfbda5ad019ee763c59
Nattapon Sub-Anake <nattapon.subanake@gmail.com>
2019-10-20 15:17:43 +0700

    QuoteList and QuoteCard

--- lib/style.dart
+++ lib/style.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 const LargeTextSize = 22.0;
-const MediumTextSize = 18.0;
+const MediumTextSize = 14.0;
 const SmallTextSize = 12.0;
 
 const String FontNameDefault = 'Kanit';
